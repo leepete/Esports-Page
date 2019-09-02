85edfa43615a8ee4b51c207f8b5303f897d9eee4
Peter <peterly@hotmail.co.nz>
2019-09-02 20:15:39 +1200

    fixing heroku deployment issues

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,7 +2,26 @@ const hamburger = document.querySelector('.hamburger');
 // const navbar = document.querySelector('.navbar');
 const navbarOptions = document.querySelector('.navbar-ul-options'); //queryselect all ul?
 
+let isNavToggled = false;
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle("change"); //temp - do changes later
     navbarOptions.classList.toggle('nav-bar-toggle');
+    isNavToggled = true;
 });
+
+// navbarOptions.addEventListener('click', (e) => {
+//     console.log(e.target);
+//     if(!e.target){
+//         console.log("yes");
+//     }
+// });
+
+// Close nav bar when clicking out of the navbar - TEMP WILL FIX
+const b = document.querySelectorAll('section');
+b.forEach((gett) =>{ 
+    gett.addEventListener('click', (e) => {
+        // console.log(e);
+        if(isNavToggled) navbarOptions.classList.toggle('nav-bar-toggle');
+    });
+})
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,44 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Wellington Esports Association' });
 });
 
+/* GET About Us page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Us' });
+});
+
+/* GET Registration page. */
+router.get('/registration', function(req, res, next) {
+  res.render('index', { title: 'Registration' });
+});
+
+/* GET Leagues page. */
+router.get('/leagues', function(req, res, next) {
+  res.render('index', { title: 'Leagues' });
+});
+
+/* GET Events page. */
+router.get('/events', function(req, res, next) {
+  res.render('index', { title: 'Events' });
+});
+
+/* GET Regulations page. */
+router.get('/regulations', function(req, res, next) {
+  res.render('index', { title: 'Regulations' });
+});
+
+/* GET Media page. */
+router.get('/media', function(req, res, next) {
+  res.render('index', { title: 'Media' });
+});
+
+/* GET  page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Us' });
+});
+
+// /* GET  page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: '' });
+// });
+
 module.exports = router;
