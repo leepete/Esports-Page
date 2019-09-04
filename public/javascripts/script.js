0b20e68841987178a4b2ee43d1e7e2e41cae359f
Peter <peterly@hotmail.co.nz>
2019-09-04 15:06:17 +1200

    fixed navbar toggle and made a smoother transition

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,27 +1,24 @@
 const hamburger = document.querySelector('.hamburger');
-// const navbar = document.querySelector('.navbar');
-const navbarOptions = document.querySelector('.navbar-ul-options'); //queryselect all ul?
+const slider = document.querySelector('.slider');
 
 let isNavToggled = false;
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle("change"); //temp - do changes later
-    navbarOptions.classList.toggle('nav-bar-toggle');
-    isNavToggled = true;
-});
+hamburger.addEventListener('click', hamburgerAnimation, false);
+slider.addEventListener('click', toggleSlider, false);
 
-// navbarOptions.addEventListener('click', (e) => {
-//     console.log(e.target);
-//     if(!e.target){
-//         console.log("yes");
-//     }
-// });
+function hamburgerAnimation(){
+    hamburger.classList.toggle('animate'); //temp - do changes later
+    toggleSlider();
+}
 
-// Close nav bar when clicking out of the navbar - TEMP WILL FIX
-const b = document.querySelectorAll('section');
-b.forEach((gett) =>{ 
-    gett.addEventListener('click', (e) => {
-        // console.log(e);
-        if(isNavToggled) navbarOptions.classList.toggle('nav-bar-toggle');
-    });
-})
+function toggleSlider(){
+    if(!isNavToggled){
+        slider.classList.remove('close');
+        slider.classList.add('open');
+        isNavToggled = true;
+    } else {
+        slider.classList.remove('open');
+        slider.classList.add('close');
+        isNavToggled = false;
+    }
+}
