f3810bf5166b56ef554930f5df35ef098c3a65b2
Peter <peterly@hotmail.co.nz>
2019-09-05 01:24:19 +1200

    Finished full implementation of Navbar on mobile view

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,24 +1,86 @@
+//Main Variables
+const body = document.body;
+
+// Header Variables
 const hamburger = document.querySelector('.hamburger');
 const slider = document.querySelector('.slider');
+const logo = document.querySelector('#navbar-logo');
+const navbarListItem = document.querySelectorAll('.slider ul li');
 
 let isNavToggled = false;
+let currentHref = '/';
 
-hamburger.addEventListener('click', hamburgerAnimation, false);
-slider.addEventListener('click', toggleSlider, false);
+function start(){
+    setupEventListeners();
+}
+
+function setupEventListeners(){
+    hamburger.addEventListener('click', hamburgerAnimation, false);
+    slider.addEventListener('click', toggleSlider, false);
+    logo.addEventListener('click', returnToPage, false);
+
+    navbarListItem.forEach(listOption => {
+        listOption.addEventListener('click', returnToPage, false);
+    });
+}
 
 function hamburgerAnimation(){
-    hamburger.classList.toggle('animate'); //temp - do changes later
+    hamburger.classList.add('animate'); 
     toggleSlider();
 }
 
 function toggleSlider(){
     if(!isNavToggled){
         slider.classList.remove('close');
+
         slider.classList.add('open');
+        body.classList.add('no-scroll');
+
         isNavToggled = true;
     } else {
         slider.classList.remove('open');
+        hamburger.classList.remove('animate');
+        body.classList.remove('no-scroll');
+
         slider.classList.add('close');
         isNavToggled = false;
     }
 }
+
+function returnToPage(event){
+    let getTarget = event.target;
+    if(getTarget.innerText === ""){
+        if(getTarget.id === "navbar-logo"){
+            currentHref = getPageHref('Home');
+        }
+    } else {
+        currentHref = getPageHref(getTarget.innerText);
+    }
+    window.location.href = currentHref;
+}
+
+function getPageHref(href){
+    switch(href){
+        case 'Home':
+            return '/';
+        case 'About Us':
+            return '/about';
+        case 'Registration':
+            return '/registration';
+        case 'Leagues':
+            return '/leagues';
+        case 'Events':
+            return '/events';
+        case 'Regulations':
+            return '/regulations';
+        case 'Media':
+            return '/media';
+        case 'Contact Us':
+            return '/contact';
+        default:
+            return '/404';
+   }
+} 
+
+
+start();
